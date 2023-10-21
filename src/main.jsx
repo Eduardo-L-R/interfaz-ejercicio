cd55caa3c8acf64f3ac137982b4dd2395509e351
Eduardo-L-R <e.leiva.rocuant@gmail.com>
2023-10-21 11:09:01 -0300

    algo

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,9 @@ import Home from "./pages/Home.jsx"
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <BrowserRouter basename="./dist"> */}
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
     {/* <Home /> */}
     <App />
-    </BrowserRouter>
+    {/* </BrowserRouter> */}
   </React.StrictMode>,
 )
